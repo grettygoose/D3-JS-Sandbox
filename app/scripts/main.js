05d91c0ac1afaa09652ad0585a6ef6cebaedfcd9
Grettygoose <gheinric@fredhutch.org>
2017-05-29 17:24:05 -0700

    playing around

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,52 @@
-console.log('\'Allo \'Allo!');
+"use strict";
+
+var symbolGenerator = d3.symbol()
+    .size(500);
+
+var symbolTypes = ['symbolCircle', 'symbolDiamond', 'symbolSquare', 'symbolTriangle'];
+
+var xScale = d3.scaleLinear()
+    .domain([0, symbolTypes.length])
+    .range([70, 400]);
+
+var colors = ["red", "green", "blue", "yellow"];
+
+d3.select('g')
+    .selectAll('path')
+    .data(symbolTypes)
+    .enter()
+    .append('path')
+    .style('opacity', 0)
+    .transition()
+        .delay(function (d, i) { return i * 1000 })
+        .duration(1000)
+        .style('fill', function (d, i) { return colors[i] })
+        .style('opacity', 1)
+
+    .attr('transform', function (d, i) {
+        return 'translate(0, ' + xScale(i) + ')';
+    })
+    .attr('d', function (d) {
+        symbolGenerator
+            .type(d3[d]);
+
+        return symbolGenerator();
+    });
+
+d3.select('g')
+    .selectAll('text')
+    .data(symbolTypes)
+    .enter()
+    .append('text')
+
+    .attr('transform', function (d, i) {
+        return 'translate(120, ' + xScale(i) + ')';
+    })
+    .text(function (d) {
+        return 'this is a ' + d;
+    });
+
+
+
+
+
